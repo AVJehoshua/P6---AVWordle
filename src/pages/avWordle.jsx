cbf59db83727970041f92bc2609f77e05397a269
Jehoshua <137894662+AVJehoshua@users.noreply.github.com>
2024-07-29 20:08:16 +0100

    minor changes

--- src/pages/avWordle.jsx
+++ src/pages/avWordle.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 export const AVWordlePage = () => {
-  // let [word, setWord] = useState([]);
-  document.title = "AV Guess"
+  let [word, setWord] = useState();
+  let [letter, setLetter] = useState();
+  let [position, setPosition] = useState();
+  let [isSubmit, setIsSubmit] = useState(false)
+  document.title = "AVGuess";
 
   const fetchWord = async () => {
     try {
@@ -13,27 +16,57 @@ export const AVWordlePage = () => {
 
       console.log("fetched data word: ", data[0]); // returns pending promise // fixed, by adding await statement next to response ^
       // console.log("fetched data list: ", data); // returns word in list, above line returns word
-      // setWord(data[0]);
+      setWord(data[0]);
     } catch (error) {
       console.log("error fetching all data: ", error);
     }
   };
+  const handleChange = (e) => {
+    setIsSubmit(true)
+    try {
+      const value = e.target.value;
+    
+      if ( value === "s") {
+        setLetter(value);
+      } else if ( value === 1) {
+        setPosition(value);
+      }
+      console.log('letter', letter)
+      console.log('position', position)
+    } catch (error) {
+      console.log('Error submitting:', error)
+    } finally {
+      setIsSubmit(false)
+    }
+  }
+  
+
 
   return (
     <>
-      <h1> AVWordle Guess </h1>
-      <p> Here to help with your wordle guesses </p>
-      <button onClick={fetchWord}> Click me to get your wordle</button>
-      {/* {word.map((data) => {
-        return (
-          <h1 className="word" key={data.id}> {data} </h1>
-        );
-      })}; */}
+      <div className="heading">
+        <h1> AVWordle Guess </h1>
+      </div>
+      <div className="paragraph">
+        <p> Need help with your wordle opening?</p>
+      </div>
+      <button onClick={fetchWord}> Click me  </button>
+      <div className="result">
+        <h3>Your word is {word} </h3>
+      </div>
+
+      <p>Input your letters, below</p>
+      <input></input> 
+      <br></br>
+      <br></br>
+      <input type="submit" onClick={handleChange}></input>
+
+      <p>Your letter was: {letter}</p>
+      <p>It's at position: {position}</p>
 
 
-      {/* <p>Your word is {word}</p> */}
     </>
   );
-};
+}; 
 
 export default AVWordlePage;
